27234cc10ce876f1eca64f978887ef26b592d4e1
Marcus Specht <me@masp.io>
2021-06-18 01:44:05 +0200

    trying to make tests work

--- src/test/src/query.test.ts
+++ src/test/src/query.test.ts
@@ -9,27 +9,27 @@ import {
 } from "testyts";
 
 import { assert } from "console";
-// import { toUrlQuery, parseUrlQuery } from "../src/query";
+export { query } from "@picibird/shared";
 
-// const sampleQuery = {
-//   foo: {
-//     bar: "baz",
-//   },
-// };
+const sampleQuery = {
+  foo: {
+    bar: "baz",
+  },
+};
 
-// const sampleQuery2 = {
-//   foo: {
-//     bar: "baz2",
-//   },
-// };
+const sampleQuery2 = {
+  foo: {
+    bar: "baz2",
+  },
+};
 
-// @TestSuite()
-// export class QueryTestSuite {
-//   @Test()
-//   public queryTest() {
-//     console.log("query string tests");
-//     const queryStr = toUrlQuery(sampleQuery);
-//     const query = parseUrlQuery(queryStr);
-//     expect.toBeEqual(4, 4);
-//   }
-// }
+@TestSuite()
+export class QueryTestSuite {
+  @Test()
+  public queryTest() {
+    console.log("query string tests");
+    const queryStr = query.toUrlQuery(sampleQuery);
+    const queryObj = query.parseUrlQuery(queryStr);
+    expect.toBeEqual(4, 4);
+  }
+}
